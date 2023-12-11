2cc7c9d76550c1575a4f6c97644ca4fb1a42cdaa
Mariam Jioshvili <128077918+Jioshvili@users.noreply.github.com>
2023-12-11 23:58:16 +0400

    Add files via upload

---
+++ mywork/animal/serializers.py
@@ -0,0 +1,8 @@
+# animal/serializers.py
+from rest_framework import serializers
+from .models import Animal
+
+class AnimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Animal
+        fields = '__all__'
